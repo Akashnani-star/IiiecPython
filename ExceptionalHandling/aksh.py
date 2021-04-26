2a0bb126aeeac9ef4429e31495ed11547bed4ca2
Akashnani-star <dhanviakash1234@gmail.com>
2021-04-26 17:45:52 +0530

    added exceptions in exception handling

--- ExceptionalHandling/aksh.py
+++ ExceptionalHandling/aksh.py
@@ -1,16 +1,23 @@
 print("Enter ur choices = ",end="")
 
-inp = input()
+try:
+	inp = input()
+	if int(inp)==1:
+	    print("1")
+	elif int(inp)==2:
+	    print("2")
+	elif int(inp)==3:
+	    print("3")
+	else:
+	    print("Sorry I can't help")
+except ValueError:
+	print("Wrong Value Entered....")
+	print("Enter only numbers....")
+except:
+	print("mail to developer")
 
-if int(inp)==1:
-    print("1")
-elif int(inp)==2:
-    print("2")
-elif int(inp)==3:
-    print("3")
-else:
-    print("Sorry I can't help")
-    
-print("mailto..")
+finally:
+	print("mailto..")
 
-print("bye")
+	print("bye")
+
